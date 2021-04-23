15a2b7db0fc4066c1ae45f1714b4f95727d5a7ad
sbigelo <sean.bigelow1@gmail.com>
2021-04-23 14:12:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, an) {
+let r = []
+
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i]
+        r.push(an(element))
+    }
+return r
+}
+
+function reduce(array, an, optional) {
+    let a = (!!optional) ? optional : array[0]
+    let b = (!!optional) ? 0 : 1 
+    for (; b < array.length; b++ ) {
+        a = an(array[b], a)
+    }
+    return a
+}
